ea6a66424b9d69ab1b370d77d247b7c659e03d52
Lam Ngo <lam.ngo.qu@gmail.com>
2017-12-10 01:19:34 +0200

    Modified UI for post_index, add idea part

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -4,7 +4,12 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { fetchPosts } from "../actions";
 
-
+const customStyle = {
+  addIdea: {
+    padding: "40px 0px 40px 0px",
+    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+  }
+};
 class PostsIndex extends Component {
   constructor(props){
     super(props);
@@ -22,12 +27,12 @@ class PostsIndex extends Component {
   searchHandler(event){
     let searchingFor = _.filter(this.props.posts, (post) => post.name.includes(event.target.value) || post.phoneNumber.includes(event.target.value));
 
-    this.setState({ 
+    this.setState({
       term: event.target.value,
       currentPeople: searchingFor
     });
   }
-  
+
   renderPosts() {
     const people = (this.state.currentPeople) ? this.state.currentPeople : this.props.posts;
     return _.map(people, (post) => {
@@ -36,28 +41,35 @@ class PostsIndex extends Component {
           <Link to={`/posts/${post._id}`}>
             {post.name}<br/>
             {post.gender}<br/>
-            
+
           </Link>
-          
+
         </li>
       );
     });
   }
-  
+
   render() {
     const { term }= this.state;
     return (
       <div>
-        
-        <div className="text-xs-right">
-        <form>
-            <input type="text" onChange= {this.searchHandler}
-            value={term}/>
-          </form>
-          <Link className="btn btn-primary" to="/posts/new">
-            Add your idea
-          </Link>
+
+        <div style={customStyle.addIdea}>
+
+          <input type="text" className="text-field" onChange={this.searchHandler}
+            value={term} placeholder="Type your idea here..."/>
+
+          <select className="text-field">
+            <option>Category 1</option>
+            <option>Category 2</option>
+          </select>
+          <div className="button button-primary">
+            <Link className="button-text" to="/posts/new">
+              ADD NEW IDEA
+            </Link>
+          </div>
         </div>
+
         <h3>Posts</h3>
         <ul className="list-group">
           {this.renderPosts()}
