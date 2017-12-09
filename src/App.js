dd23bddbbb79b30099159b5866175d1906f180a7
Lam Ngo <lam.ngo.qu@gmail.com>
2017-12-10 00:30:33 +0200

    General UI added

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import reducers from "./reducers";
 import PostsIndex from "./components/posts_index";
 import PostsNew from "./components/posts_new";
 import PostsShow from "./components/posts_show";
-
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Switch>
-        
-          <Route path="/posts/new" component={PostsNew} />
-          <Route path="/posts/:id" component={PostsShow} />
-          <Route path="/" component={PostsIndex} />
-        </Switch>
+        <div className="background">
+          <div className="content-wrapper">
+            <Switch>
+              <Route path="/posts/new" component={PostsNew} />
+              <Route path="/posts/:id" component={PostsShow} />
+              <Route path="/" component={PostsIndex} />
+            </Switch>
+          </div>
+        </div>
       </Router>
     );
   }
